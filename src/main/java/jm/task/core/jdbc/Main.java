d21eabd95203d5c780718fc8a1f8b724c023f86f
Alabala-commits <66037047+Alabala-commits@users.noreply.github.com>
2021-07-23 14:57:28 +0300

    to check the load class Driver

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+
+//        User user1 = new User("Gordan", "Freeman", (byte) 27);
+//        User user2 = new User("Bobby", "Dig", (byte) 31);
+//        User user3 = new User("Lione", "Poll", (byte) 24);
+//        User user4 = new User("zzz", "xxx", (byte) 27);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+                                                    //  127.0.0.1
+    private static final String URL      = "jdbc.mysql://localhost:3306/testbase";
+    private static final String LOGIN    = "root";
+    private static final String PASSWORD = "Password";
+
+    public static Connection connection() throws SQLException, ClassNotFoundException {
+        return DriverManager.getConnection(URL, LOGIN, PASSWORD);
+    }
 }
