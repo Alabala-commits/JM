5d78df3c23e39dc553c75a92c5afedaeb9318572
Alabala-commits <66037047+Alabala-commits@users.noreply.github.com>
2021-07-26 17:30:34 +0300

    to check 1.1.3

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -32,7 +32,7 @@ public class User {
     public String toString() {
         return "User{" + "id = " + id + ", name = " + name +
                 ", lastName = " + lastName + ", age = " + age + "}";
-    }1
+    }
 
     public Long getId() {
         return id;
